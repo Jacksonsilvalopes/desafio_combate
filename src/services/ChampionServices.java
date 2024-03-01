e059b947bd9ba7335b2dc150a46d75b48224e3fc
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2024-03-01 06:47:36 -0300

    Champion Service logic adjustment

--- src/services/ChampionServices.java
+++ src/services/ChampionServices.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ChampionServices {
 
     private List<Champion> championList;
+
     public ChampionServices(List<Champion> championList) {
         this.championList = championList;
 
@@ -19,29 +20,36 @@ public class ChampionServices {
 
         for (int i = 0; i < n; i++) {
 
-           int turno = i + 1;
+            int turno = i + 1;
 
             champion1.takeDamage(champion2);
             champion2.takeDamage(champion1);
 
             if (champion1.getLife() > 0 && champion2.getLife() > 0) {
-                summary( turno, champion1,  champion2);
+                summary(turno, champion1, champion2);
 
             } else {
-                summary( turno, champion1,  champion2);
+                summary(turno, champion1, champion2);
+                gameOver();
 
                 return;
             }
 
         }
 
+        gameOver();
+
     }
 
-    public void summary(int turno, Champion champion1, Champion champion2){
+    private void summary(int turno, Champion champion1, Champion champion2) {
         System.out.println("Resultado do turno " + turno + ":");
         System.out.println(champion1.status());
         System.out.println(champion2.status());
         System.out.println();
+
+    }
+
+    private void gameOver() {
         System.out.println("FIM DO COMBATE ");
     }
 
